9c01ac7af1d598e017fbd4537804d0032823243f
kauan <kauanalexandre2003@outlook.com>
2024-11-23 21:07:09 -0300

    Ajustado nome do projeto e pronto para solucionar a prova

---
+++ app/src/main/java/br/edu/up/AppRoom8828952782/MainActivity.kt
@@ -0,0 +1,34 @@
+package br.edu.up.AppRoom8828952782
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.up.AppRoom8828952782.ui.theme.InventoryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InventoryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InventoryApp()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/edu/up/AppRoom8828952782/ui/home/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package br.edu.up.AppRoom8828952782.ui.home
+
+import androidx.lifecycle.ViewModel
+import br.edu.up.AppRoom8828952782.data.Item
+
+
+/**
+ * ViewModel to retrieve all items in the Room database.
+ */
+class HomeViewModel : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * Ui State for HomeScreen
+ */
+data class HomeUiState(val itemList: List<Item> = listOf())
